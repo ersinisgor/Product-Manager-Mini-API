11a0670fc73085a94ccaed0c29bedfa52eff8ffb
ersinisgor <ersinisgor@hotmail.com>
2025-05-06 12:06:45 +0300

    Refactor: Replace weather API with product management API

--- Product Manager Mini API/Program.cs
+++ Product Manager Mini API/Program.cs
@@ -26,26 +26,12 @@ namespace Product_Manager_Mini_API
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast")
-            .WithOpenApi();
+       
+            app.MapGet("/products", () => "Get all products");
+            app.MapGet("/products/{id}", () => "Get products by id");
+            app.MapPost("/products/{id}", () => "Create product");
+            app.MapPut("/products/{id}", () => "Update product");
+            app.MapDelete("/products/{id}", () => "Delete product");
 
             app.Run();
         }
